27d1164e96b7f6cda37d74fb382f09d9d2c8ec93
Wyll <slickqocd@gmail.com>
2026-03-14 19:45:10 +0700

    a

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Support\Facades\Artisan; 
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/clear', function () {
+    Artisan::call('optimize:clear');
+    return 'Đã dọn dẹp toàn bộ Cache thành công!';
+});
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
