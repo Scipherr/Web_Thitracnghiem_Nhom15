3114e52de5739d58d87fe1914d93d03bc79c15e6
Wyll <slickqocd@gmail.com>
2026-03-14 13:14:48 +0700

    rollback

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/{any}', function () {
-    $path = public_path('index.html');
-    if (file_exists($path)) {
-        return file_get_contents($path);
-    }
+Route::get('/', function () {
     return view('welcome');
-})->where('any', '.*');
+});
